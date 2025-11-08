c7039c387322cab9df396e3514d86456cee337b0
Pramtoxz <pramuditometra@gmail.com>
2025-11-08 15:20:54 +0700

    title

--- resources/js/pages/Home.tsx
+++ resources/js/pages/Home.tsx
@@ -1,5 +1,8 @@
 import SpaceLanding from "@/components/home/space-landing"
+import {Head} from "@inertiajs/react";
 
 export default function Home() {
+     <Head title="AL-Biruni">
+             </Head>
   return <SpaceLanding />
 }
