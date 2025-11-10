4bc5681def0d18029a931e9d64df2bc757ed567f
Pramtoxz <pramuditometra@gmail.com>
2025-11-10 10:12:08 +0700

    gambar pulaang

---
+++ public/build/assets/spinner-CkcTTG-E.js
@@ -0,0 +1,1 @@
+import{j as s}from"./app-8GjJj8Bc.js";import{c as i}from"./utils-D-KgF5mV.js";import{L as o}from"./loader-circle-BvEQE4pi.js";function m({className:r,...a}){return s.jsx(o,{role:"status","aria-label":"Loading",className:i("size-4 animate-spin",r),...a})}export{m as S};
