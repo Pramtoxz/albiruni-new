c6fbc80ae3b5abd6f4760c56840ee6eea0684e98
Pramtoxz <pramuditometra@gmail.com>
2025-11-12 14:22:02 +0700

    cache

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -41,9 +41,16 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" href="/favicon.ico?v={{ config('app.version', '1.0') }}" sizes="any">
+        <link rel="icon" href="/favicon.svg?v={{ config('app.version', '1.0') }}" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png?v={{ config('app.version', '1.0') }}">
+        
+        {{-- Open Graph / Social Media Meta Tags --}}
+        <meta property="og:image" content="{{ url('/apple-touch-icon.png') }}" />
+        <meta property="og:image:secure_url" content="{{ url('/apple-touch-icon.png') }}" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="180" />
+        <meta property="og:image:height" content="180" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
